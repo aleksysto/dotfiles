6c18b01b637a95f6b143ce203787b8f8c7227e56
Aleksy Stocki <aleksy.stocki@bioseco.com>
2026-06-23 10:27:16 +0200

    lualine update

--- private_dot_config/nvim/lua/plugins/line.lua
+++ private_dot_config/nvim/lua/plugins/line.lua
@@ -4,8 +4,27 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				-- ... other configuration
 				theme = "auto", -- Can also be "auto" to detect automatically.
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						newfile_status = true, -- Display new file status (new file means no write after created)
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+
+						shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						-- for other components. (terrible name, any suggestions?)
+						-- It can also be a function that returns
+						-- the value of `shorting_target` dynamically.
+					},
+					{ "searchcount" },
+				},
 			},
 		})
 	end,
